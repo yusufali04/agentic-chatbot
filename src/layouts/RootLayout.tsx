b745343c6357de903ca7b2fd9940517420c1f53f
Yusuf <yusufali.5094@gmail.com>
2026-02-23 23:43:04 +0530

    design chat layout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
     return (
-        <main className="bg-neutral-900 flex items-center justify-center p-4 border border-white-2 w-full h-screen">
+        <main className="bg-neutral-900 p-4 w-full h-screen">
             <Outlet />
         </main>
     );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 
 const Home = () => {
     return (
-        <div className="text-white container mx-auto max-w-4xl p-4 border-2 h-full">
+        // hide scrollbar but still allow scrolling 
+        <div className="text-white container mx-auto max-w-4xl p-4 h-full overflow-y-auto hide-scroll pb-45">
 
             {/* messages goes here */}
             <div className="my-6 bg-neutral-800 p-3 rounded-xl ml-auto w-fit">
@@ -12,13 +13,16 @@ const Home = () => {
                 I am fine, How are you doing?
             </div>
             {/* bottom text area goes here */}
-            <div className="bg-neutral-800 rounded-xl p-4 mt-6">
-                <textarea className="w-full p-3 rounded-xl text-white resize-none outline-none" placeholder="Type your message here..." rows={1} />
-                <div className="flex justify-end items-center">
-                    <button className="bg-white text-black px-4 py-2 rounded-xl mt-2 cursor-pointer hover:bg-gray-200">Send</button>
+            <div className="fixed bottom-5 left-0 right-0 mx-auto max-w-4xl">
+                <div className="bg-neutral-800 rounded-xl p-4 mt-6">
+                    <textarea className="w-full p-3 rounded-xl text-white resize-none outline-none custom-scroll" placeholder="Type your message here..." rows={2} />
+                    <div className="flex justify-end items-center">
+                        <button className="bg-white text-black px-4 py-2 rounded-xl mt-2 cursor-pointer hover:bg-gray-200">Send</button>
+                    </div>
                 </div>
             </div>
         </div>
+
     );
 }
 
